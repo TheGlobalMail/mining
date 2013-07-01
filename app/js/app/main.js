c8287edb37857afacf9551450908f346867ab5ad
Mark Finger <markfinger@gmail.com>
2013-07-01 17:33:26 +1000

    Added main loading screen.

--- app/js/app/main.js
+++ app/js/app/main.js
@@ -8,7 +8,19 @@ define([
 ], function($, _, events, layout, media, scroll) {
   'use strict';
 
+  var body;
+
+  var setBindings = function() {
+    events.on('scroll:end', function() {
+      body.removeClass('loading');
+    });
+  };
+
   var init = function() {
+    body = $('body');
+
+    setBindings();
+
     // Initialise modules
     layout.init();
     media.init();
