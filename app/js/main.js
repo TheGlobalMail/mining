4ff1ae4c8a14627b4bbd469354cce03ff02597bd
B.J. Rossiter <b.j.rossiter@gmail.com>
2013-07-08 15:16:37 +1000

    Added two basic data vis

--- app/js/main.js
+++ app/js/main.js
@@ -8,7 +8,8 @@ require.config({
     fatcontroller: './libs/fatcontroller',
     soundManager: '../components/soundmanager/script/soundmanager2',
     videojs: '../components/video.js/video',
-    bootstrapSwitch: '../components/bootstrap-switch/static/js/bootstrapSwitch'
+    bootstrapSwitch: '../components/bootstrap-switch/static/js/bootstrapSwitch',
+    highcharts: '../components/highcharts.com/js/highcharts.src'
   },
   shim: {
     backbone: {
@@ -23,6 +24,10 @@ require.config({
     },
     bootstrapSwitch: {
       deps: ['jquery']
+    },
+    highcharts: {
+      deps: ['jquery'],
+      exports: 'Highcharts'
     }
   }
 });
